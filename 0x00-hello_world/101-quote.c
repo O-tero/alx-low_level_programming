edd13224b85001ff8c769ae260b55c42b4a99dbf
O-tero <oteronickbrian@gmail.com>
2022-03-10 16:40:00 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 	return (1);
 }
