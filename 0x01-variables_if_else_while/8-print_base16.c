7b97066b7e4609d16ad50172ed361529fa4ccc51
O-tero <oteronickbrian@gmail.com>
2022-03-13 12:20:00 -0700

    Print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 
         for (ch = '0'; ch <= '9'; ch++)
 	{
-		putchar(ch)
+		putchar(ch);
 	}
-	for (ch = 'a;ch <= 'f'; ch++)
+	for (ch = 'a';ch <= 'f'; ch++)
 	{
-		putchar(ch)
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
