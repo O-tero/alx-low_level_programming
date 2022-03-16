bec5449b39e04cce3f7bb69c6a0cbbcc0565f0af
O-tero <oteronickbrian@gmail.com>
2022-03-16 15:51:31 +0300

    alx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+			i++;
+	}
+}
