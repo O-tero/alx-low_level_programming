758bc80356884dadb9d740cf5e6216d953923535
O-tero <oteronickbrian@gmail.com>
2022-06-15 04:25:11 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -4,12 +4,12 @@
  *_strchr - Returns a pointer to the first occurrence
  *of the character c in the string s, or NULL if the
  *character is not found
- * 
+ *
  *@s:string targeted
- *@c:character targeted
- * 
+ *@c:character targeted 
+ *
  *Return: returns pointer to first occcurence of c
- * */
+ */
 char *_strchr(char *s, char c)
 {
 	int i;
