67e64c61ebd30a8489218bf1430f25c22ab2bc2c
O-tero <oteronickbrian@gmail.com>
2022-07-01 13:20:23 -0700

    C - Hash tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,133 @@
+nes (29 sloc)  483 Bytes
+
+
+
+#include "hash_tables.h"
+
+
+
+/**
+ *
+ *  * hash_table_delete - deletes the hash table
+ *
+ *   * @ht: the hash table of hash_table_t
+ *
+ *    * Return: nothing
+ *
+ *     */
+
+void hash_table_delete(hash_table_t *ht)
+
+{
+
+		int i;
+
+			hash_node_t *node, *tmp;
+
+
+
+				if (ht == NULL)
+
+							return;
+
+
+
+					for (i = 0; i < (int)ht->size; i++)
+
+							{
+
+										node = ht->array[i];
+
+												if (!node)
+
+																continue;
+
+														while (node)
+
+																	{
+
+																					free(node->value);
+
+																								free(node->key);
+
+																											tmp = node->next;
+
+																														free(node);
+
+																																	node = tmp;
+
+																																			}
+
+															}
+
+						free(ht->array);
+
+							free(ht);
+
+}i lines (29 sloc)  483 Bytes
+
+
+
+#include "hash_tables.h"
+
+
+
+/**
+ *
+ *  * hash_table_delete - deletes the hash table
+ *
+ *   * @ht: the hash table of hash_table_t
+ *
+ *    * Return: nothing
+ *
+ *     */
+
+void hash_table_delete(hash_table_t *ht)
+
+{
+
+		int i;
+
+			hash_node_t *node, *tmp;
+
+
+
+				if (ht == NULL)
+
+							return;
+
+
+
+					for (i = 0; i < (int)ht->size; i++)
+
+							{
+
+										node = ht->array[i];
+
+												if (!node)
+
+																continue;
+
+														while (node)
+
+																	{
+
+																					free(node->value);
+
+																								free(node->key);
+
+																											tmp = node->next;
+
+																														free(node);
+
+																																	node = tmp;
+
+																																			}
+
+															}
+
+						free(ht->array);
+
+							free(ht);
+
+}
