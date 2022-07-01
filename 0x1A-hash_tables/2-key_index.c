65bcda6b580d8c3be79e5d3a0b986c093336425f
otero nick <99287159+O-tero@users.noreply.github.com>
2022-07-01 23:06:38 +0300

    C - Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,29 +1,15 @@
-nclude "hash_tables.h"
-
-
+#include "hash_tables.h"
 
 /**
- *
- *  * key_index - retrieves the index of a key
- *
- *   * @key: the key to find it's index
- *
- *    * @size: size of the array of the hash table
- *
- *     * Return: index in which key/value pair exists in the array of hash table
- *
- *      */
-
+ * key_index - retrieves the index of a key
+ * @key: the key to find it's index
+ * @size: size of the array of the hash table
+ * Return: index in which key/value pair exists in the array of hash table
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-
 {
+	unsigned long int hash_value;
 
-		unsigned long int hash_value;
-
-
-
-			hash_value = hash_djb2(key);
-
-				return (hash_value % size);
-
+	hash_value = hash_djb2(key);
+	return (hash_value % size);
 }
