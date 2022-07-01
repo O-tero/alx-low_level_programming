71cf5ea7519665b145980804a423c3a60879b709
otero nick <99287159+O-tero@users.noreply.github.com>
2022-07-01 23:21:08 +0300

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,133 +1,34 @@
-nes (29 sloc)  483 Bytes
-
-
+ lines (29 sloc)  483 Bytes
 
 #include "hash_tables.h"
 
-
-
 /**
- *
- *  * hash_table_delete - deletes the hash table
- *
- *   * @ht: the hash table of hash_table_t
- *
- *    * Return: nothing
- *
- *     */
-
+ * hash_table_delete - deletes the hash table
+ * @ht: the hash table of hash_table_t
+ * Return: nothing
+ */
 void hash_table_delete(hash_table_t *ht)
-
 {
+	int i;
+	hash_node_t *node, *tmp;
 
-		int i;
+	if (ht == NULL)
+		return;
 
-			hash_node_t *node, *tmp;
-
-
-
-				if (ht == NULL)
-
-							return;
-
-
-
-					for (i = 0; i < (int)ht->size; i++)
-
-							{
-
-										node = ht->array[i];
-
-												if (!node)
-
-																continue;
-
-														while (node)
-
-																	{
-
-																					free(node->value);
-
-																								free(node->key);
-
-																											tmp = node->next;
-
-																														free(node);
-
-																																	node = tmp;
-
-																																			}
-
-															}
-
-						free(ht->array);
-
-							free(ht);
-
-}i lines (29 sloc)  483 Bytes
-
-
-
-#include "hash_tables.h"
-
-
-
-/**
- *
- *  * hash_table_delete - deletes the hash table
- *
- *   * @ht: the hash table of hash_table_t
- *
- *    * Return: nothing
- *
- *     */
-
-void hash_table_delete(hash_table_t *ht)
-
-{
-
-		int i;
-
-			hash_node_t *node, *tmp;
-
-
-
-				if (ht == NULL)
-
-							return;
-
-
-
-					for (i = 0; i < (int)ht->size; i++)
-
-							{
-
-										node = ht->array[i];
-
-												if (!node)
-
-																continue;
-
-														while (node)
-
-																	{
-
-																					free(node->value);
-
-																								free(node->key);
-
-																											tmp = node->next;
-
-																														free(node);
-
-																																	node = tmp;
-
-																																			}
-
-															}
-
-						free(ht->array);
-
-							free(ht);
-
+	for (i = 0; i < (int)ht->size; i++)
+	{
+		node = ht->array[i];
+		if (!node)
+			continue;
+		while (node)
+		{
+			free(node->value);
+			free(node->key);
+			tmp = node->next;
+			free(node);
+			node = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
